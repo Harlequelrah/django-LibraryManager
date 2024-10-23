2958c9c3aefa47609a5a5714dc347132d525b121
Harlequelrah <degbovimax19@gmail.com>
2024-10-23 01:58:21 +0000

    creation du model genre

--- LibraryManager/books/admin.py
+++ LibraryManager/books/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Book
+from .models import Book,Genre
 class AdminBook(admin.ModelAdmin):
-    list_display = ('title', 'author','label', 'published_year', 'cover_image')
+    list_display = ('title', 'author', 'publication_date', 'stars')
+
+class AdminGenre(admin.ModelAdmin):
+    list_display = ('name','description')
 admin.site.register(Book,AdminBook)
+admin.site.register(Genre, AdminGenre)
--- LibraryManager/books/models.py
+++ LibraryManager/books/models.py
@@ -5,9 +5,20 @@ class Book(models.Model):
     title=models.fields.CharField(verbose_name='titre',max_length=150)
     label=models.fields.CharField(verbose_name='description',max_length=100)
     author=models.fields.CharField(max_length=150,verbose_name='auteur')
-    published_year=models.fields.IntegerField(verbose_name='année de publication')
-    cover_image=models.ImageField(verbose_name='couverture')
+    publication_date=models.fields.DateTimeField(verbose_name='année de publication')
+    cover_image=models.ImageField(verbose_name='couverture',null=True)
+    available=models.fields.BooleanField(verbose_name='disponibilité',default=True)
+    stars=models.fields.IntegerField(default=0)
+    genre=models.ForeignKey('Genre',on_delete=models.SET_DEFAULT,default='UNKNOW',related_name='genres')
+
+    def __str__(self):
+        return self.title
 
 
+class Genre(models.Model):
+    name=models.fields.CharField(verbose_name='nom',max_length=50)
+    description=models.fields.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 
